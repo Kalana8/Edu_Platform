9f408b4da4e8bf68d5e22da417bfb739eb9412de
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2026-07-09 09:36:54 +0530

    Next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  
-  allowedDevOrigins: ['192.168.8.102', 'localhost', '127.0.0.1'],
+
+  allowedDevOrigins: [
+    "localhost",
+    "127.0.0.1",
+    ".local",
+    "192.168.*",
+    "10.0.*",
+    "172.16.*",
+  ],
 };
 
 export default nextConfig;
