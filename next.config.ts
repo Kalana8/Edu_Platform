3c478c0bd58b0aaa60e3ce48e5647debbe645e94
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2026-07-09 09:41:21 +0530

    Next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,30 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactCompiler: true,
+
+//   allowedDevOrigins: [
+//     "localhost",
+//     "127.0.0.1",
+//     ".local",
+//     "192.168.*",
+//     "10.0.*",
+//     "172.16.*",
+//   ],
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-
-  allowedDevOrigins: [
-    "localhost",
-    "127.0.0.1",
-    ".local",
-    "192.168.*",
-    "10.0.*",
-    "172.16.*",
-  ],
+  
+  allowedDevOrigins: ['192.168.8.101', 'localhost', '127.0.0.1'],
 };
 
 export default nextConfig;
+
+
